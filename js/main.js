9286a5fec3a833faf8517ffe304de864da983649
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2017-06-11 17:01:54 +0200

    project2: collections

--- js/main.js
+++ js/main.js
@@ -24,25 +24,32 @@ var Vehicle = Backbone.Model.extend({
     }
 });
 
+var Vehicles = Backbone.Collection.extend();
+
 var Car = Vehicle.extend({
     start: function() {
         console.log('Car with registration number `' + this.get('registrationNumber') + '` started.')
     }
 });
 
-var car = new Car({
-    registrationNumber: 'XLI887',
-    color: 'Blue'
+var vehicles = new Vehicles([
+    new Car({registrationNumber: 'XLI887', colour: 'Blue'}),
+    new Car({registrationNumber: 'ZNP123', colour: 'Blue'}),
+    new Car({registrationNumber: 'XUV456', colour: 'Gray'})
+]);
+
+var blueCars = vehicles.where({colour: 'Blue'});
+
+console.log('Blue cars', blueCars);
+
+var carToRemove = vehicles.findWhere({registrationNumber: 'XLI887'});
+console.log('XLI887: ', carToRemove);
+
+vehicles.remove(carToRemove);
+console.log(vehicles.length);
+
+console.log('json: ', vehicles.toJSON());
+
+vehicles.each(function(vehicle) {
+    console.log(vehicle);
 });
-
-car.unset('registrationNumber');
-
-if (!car.isValid()) {
-    console.log(car.validationError);
-}
-
-car.set('registrationNumber', 'XLI887');
-
-console.log(car.isValid());
-
-car.start();
