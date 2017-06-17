5d1d22a148f2c1a34a76d90f56f02b050c2c0d42
Daniel Nahrebecki <dnahrebecki@oroinc.com>
2017-06-17 22:01:10 +0200

    todo list project #6

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -29,11 +29,10 @@ var TodoItemView = Backbone.View.extend({
     render: function() {
         this.$el.attr("id", this.model.id);
         this.$el.toggleClass('completed', this.model.get('completed'));
-        var checked = this.model.get('completed') ? 'checked' : '';
-        var checkbox = "<input id='toggle' type='checkbox' " + checked + "/>";
-        var deleteButton = "<button id='delete'>Delete</button>";
 
-        this.$el.html(checkbox + this.model.escape("title") + deleteButton);
+        var template = $('#todoItemTemplate').html();
+        var html = Mustache.render(template, this.model.toJSON());
+        this.$el.html(html);
 
         return this;
     }
